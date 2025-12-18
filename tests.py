3b26938bebde3d54097bf860694576e31004eac6
Hammad Ali Rao <hammadalirao44@gmail.com>
2025-12-18 15:37:16 +0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-# Headless Chrome for GitHub Actions
 options = Options()
-options.headless = True
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
 driver = webdriver.Chrome(options=options)
+
 
 # Open the HTML file (adjust path if needed)
 driver.get("file:///C:/Users/user/Desktop/Github/demo-calculator/index.html")
